827c43caff9b022010c756d551df3f4e6683776f
KellenSchmitz <86166117+KellenSchmitz@users.noreply.github.com>
2021-09-01 19:53:42 -0700

    extended code to calculate total votes received +%

--- PyPoll.py
+++ PyPoll.py
@@ -1,34 +1,58 @@
 import csv
 import os
 
-
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter variable
+total_votes = 0
+
+# create new list to count unique names in column 3 of the data
+candidate_options=[]
+
+# create a new dictionary to count up each candidate's votes
+candidate_votes={}
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # Read the csv file and print the data
     file_reader = csv.reader(election_data)
 
-    # Print each row of data from the csv file
+    next(file_reader) # this just skips the first row in csv file
+
+    # Count each row of data from the csv file
     for row in file_reader:
-        print(row)
+        total_votes +=1
+
+        # The candidate name from each row is in column 3 (index 2)
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options: 
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+            # initialize candidate_votes variable to start at zero
+            candidate_votes[candidate_name] = 0
+
+        # count each vote per candidate
+        candidate_votes[candidate_name] += 1
+
+    ### Calculate the % of votes for each candidate
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate_name}: received {vote_percentage:.2f}% of the total vote.")
 
 
 
 
 
-#####
-# Create a new file and filename variable for the path to the new file
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# use the with command and open() function and "w" write to write data to the new file
-with open(file_to_save, "w") as txt_file:
 
-    #write data to the file
-    txt_file.write("Test 1, test 2...")    
 
-    txt_file.write("Arapahoe\nDenver\nJefferson")
 
+# Print the results
+    # print(total_votes)
+    # print(candidate_options)
+    # print(candidate_votes)
